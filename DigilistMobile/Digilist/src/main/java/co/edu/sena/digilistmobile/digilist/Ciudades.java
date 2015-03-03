05be7b7597d07b6baf3396803cd15275e7bbbf61
Edanesar0 <eaespejoa@misena.edu.co>
2015-03-02 23:49:29 -0500

    Commit

--- DigilistMobile/Digilist/src/main/java/co/edu/sena/digilistmobile/digilist/Ciudades.java
+++ DigilistMobile/Digilist/src/main/java/co/edu/sena/digilistmobile/digilist/Ciudades.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -25,7 +26,7 @@ import co.edu.sena.digilistmobile.digilist.dao.CityDAO;
  */
 public class Ciudades extends SherlockActivity implements View.OnClickListener {
     private ProgressBar pbCiudades;
-    private TableLayout tCiudades;
+    private Spinner sCiudades;
     private ScrollView svCiudades;
     static Typeface font;
     CityDAO ciudades;
@@ -56,7 +57,7 @@ public class Ciudades extends SherlockActivity implements View.OnClickListener {
 
         protected void onPreExecute() {
             svCiudades = (ScrollView) findViewById(R.id.svCiudad);
-            pbCiudades = (ProgressBar) findViewById(R.id.pbCiudad);
+
             pbCiudades.setVisibility(View.VISIBLE);
             svCiudades.setVisibility(View.INVISIBLE);
 
@@ -75,39 +76,11 @@ public class Ciudades extends SherlockActivity implements View.OnClickListener {
         }
     }
     public void cargarCiudades(){
-        tCiudades = (TableLayout) findViewById(R.id.tlCiudad);
+        sCiudades = (Spinner) findViewById(R.id.sCiudades);
         //tCiudades.setStretchAllColumns(true);
         //tCiudades.setShrinkAllColumns(true);
-        TextView lblNombre = (TextView) findViewById(R.id.lblNombreCiudad);
-        lblNombre.setTypeface(font);
-        ArrayList<String> materiales = ciudades.consultarCiudades("");
-        int count = 0;
-        if (materiales.size() != 0) {
-            for (int i = 0; i <= materiales.size() - 2; i = i + 2) {
-                final TableRow tr = new TableRow(Ciudades.this);
 
-                if (count % 2 != 0) {
-                    tr.setBackgroundResource(R.drawable.row_selector_r);
-                    //tr.setBackgroundColor(Color.argb(15, 203, 47, 23));
-                } else {
-                    tr.setBackgroundResource(R.drawable.row_selector_w);
-                    //tr.setBackgroundColor(Color.WHITE);
-                }
-                final TextView txtNombre = new TextView(Ciudades.this);
-                txtNombre.setTypeface(font);
-                txtNombre.setId(Integer.parseInt(materiales.get(i)));
-                txtNombre.setText(materiales.get(i + 1));
-                txtNombre.setLines(3);
-                txtNombre.setTypeface(font);
-                txtNombre.setGravity(Gravity.CENTER);
-                //txtProducto.setTextSize(20);
-                tr.addView(txtNombre);
-                tCiudades.addView(tr, new TableLayout.LayoutParams(
-                        TableLayout.LayoutParams.WRAP_CONTENT,
-                        TableLayout.LayoutParams.FILL_PARENT));
-            count++;
-            }
         }
     }
-}
+
 
